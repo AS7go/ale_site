6f2c272d6ec4c026401318fe43a42295983d1f19
Aleksandr <sh17aleksandr@gmail.com>
2024-04-05 14:26:11 +0300

    old and optimal index.php foreach

--- www/index.php
+++ www/index.php
@@ -2,106 +2,91 @@
 <html>
 
 <head>
-     <title>PHP Form with PDO</title>
+    <title>PHP Form with PDO</title>
 
-     <!-- Bootstrap CSS -->
-     <!-- <link rel="stylesheet" https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"> -->
-     <link href="/bootstrap4_6_2/css/bootstrap.min.css" rel="stylesheet">
-     <link rel="stylesheet" href="/bootstrap4_6_2/css/fontawesome-free-5.15.4-web/css/all.css">
+    <!-- Bootstrap CSS -->
+    <link href="/bootstrap4_6_2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/bootstrap4_6_2/css/fontawesome-free-5.15.4-web/css/all.css">
 
+    <!-- Подключение стилей из отдельного файла -->
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
-     <div class="container">
-          <h1 class="mb-3 mt-3">Test PHP code</h1>
-          <div class="container row">
-               <div class="p-1">
-                    <div class="card" style="width: 18rem;">
-                         <a href="/site1">
-                              <img src="img/crud1.png" class="card-img-top p-1" alt="...">
-                         </a>
-                         <div class="card-body">
-                              <h5 class="card-title">
-                                   <a href="/site1" class="card-link text-dark">Add to database</a>
-                              </h5>
-                              <p class="card-text">Adding and viewing data in the database</p>
-                              <a href="/site1" class="btn btn-primary">Example 1</a>
-                         </div>
-                    </div>
-               </div>
-               <div class="p-1">
-                    <div class="card" style="width: 18rem;">
-                         <a href="/site2">
-                              <img src="img/crud2.png" class="card-img-top img-fluid p-1" alt="...">
-                         </a>
-                         <div class="card-body">
-                              <h5 class="card-title">
-                                   <a href="/site2" class="card-link text-dark">CRUD</a>
-                              </h5>
-                              <p class="card-text">Create, Read, Update, Delete operations in the database.</p>
-                              <a href="/site2" class="btn btn-primary">Example 2</a>
-                         </div>
-                    </div>
-               </div>
-               <div class="p-1">
-                    <div class="card" style="width: 18rem;">
-                         <a href="/site3">
-                              <img src="https://dummyimage.com/300x200/6824b5/ffffff" class="card-img-top" alt="...">
-                         </a>
-                         <div class="card-body">
-                              <h5 class="card-title">Authentication authorization</h5>
-                              <p class="card-text">old, doesn't work Authentication authorization database</p>
-                              <a href="/site3" class="btn btn-primary">Example 3</a>
-                         </div>
-                    </div>
-               </div>
-          </div>
-          <div class="container row">
-               <div class="p-1">
-                    <div class="card" style="width: 18rem;">
-                         <a href="/site4">
-                              <img src="img/di.png" class="card-img-top p-1" alt="...">
-                         </a>
-                         <div class="card-body">
-                              <h5 class="card-title">Внедрение зависимостей DI</h5>
-                              <p class="card-text">Простой пример с использованием MySql</p>
-                              <a href="/site4" class="btn btn-primary">Example 4</a>
-                         </div>
-                    </div>
-               </div>
-               <div class="p-1">
-                    <div class="card" style="width: 18rem;">
-                         <a href="/site5">
-                              <img src="img/crud5.png" class="card-img-top" alt="...">
-                         </a>
-                         <div class="card-body">
-                              <h5 class="card-title">Авторизация и регистрация</h5>
-                              <p class="card-text">Авторизация регистрация на чистом PHP</p>
-                              <a href="/site5" class="btn btn-primary">Example 5</a>
-                         </div>
-                    </div>
-               </div>
-               <div class="p-1">
-                    <div class="card" style="width: 18rem;">
-                         <a href="/site6">
-                              <img src="https://dummyimage.com/300x200/6824b5/ffffff" class="card-img-top" alt="...">
-                              <div class="card-body">
-                         </a>
-                         <h5 class="card-title">Card title</h5>
-                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                         <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-               </div>
-          </div>
-     </div>
-     </div>
+    <div class="container">
+        <h1 class="mb-3 mt-3">Test PHP code</h1>
 
-     <script src="/bootstrap4_6_2/js/jquery.slim.min.js"></script>
-     <script src="/bootstrap4_6_2/js/bootstrap.bundle.min.js"></script>
+        <div class="container row">
+            <?php
+            // Массив данных для карточек
+            $cards = array(
+                array(
+                    "url" => "/site1",
+                    "img_src" => "img/crud1.png",
+                    "title" => "Add to database",
+                    "text" => "Adding and viewing data in the database",
+                    "btn_text" => "Example 1"
+                ),
+                array(
+                    "url" => "/site2",
+                    "img_src" => "img/crud2.png",
+                    "title" => "+++ CRUD",
+                    "text" => "Create, Read, Update, Delete operations in the database.",
+                    "btn_text" => "Example 2"
+                ),
+                array(
+                    "url" => "/site3",
+                    "img_src" => "https://dummyimage.com/300x200/6824b5/ffffff",
+                    "title" => "Authentication authorization",
+                    "text" => "old, doesn't work Authentication authorization database",
+                    "btn_text" => "Example 3"
+                ),
+                array(
+                    "url" => "/site4",
+                    "img_src" => "img/di.png",
+                    "title" => "Внедрение зависимостей DI",
+                    "text" => "Простой пример с использованием MySql",
+                    "btn_text" => "Example 4"
+                ),
+                array(
+                    "url" => "/site5",
+                    "img_src" => "img/crud53.png",
+                    "title" => "+++ Авторизация и регистрация",
+                    "text" => "Авторизация регистрация на чистом PHP, пример",
+                    "btn_text" => "Example 5"
+                ),
+                array(
+                    "url" => "/site6",
+                    "img_src" => "https://dummyimage.com/300x200/6824b5/ffffff",
+                    "title" => "Card title",
+                    "text" => "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    "btn_text" => "Go somewhere"
+                )
+            );
 
-     <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
-     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script> -->
+            // Цикл для создания карточек
+            foreach ($cards as $card) {
+                echo '<div class="p-1">';
+                echo '<div class="card" style="width: 18rem;">';
+                echo '<a href="' . $card["url"] . '">';
+                echo '<img src="' . $card["img_src"] . '" class="card-img-top p-1" alt="...">';
+                echo '</a>';
+                echo '<div class="card-body">';
+                echo '<h5 class="card-title">';
+                echo '<a href="' . $card["url"] . '" class="card-link text-dark">' . $card["title"] . '</a>';
+                echo '</h5>';
+                echo '<p class="card-text">' . $card["text"] . '</p>';
+                echo '<a href="' . $card["url"] . '" class="btn btn-primary">' . $card["btn_text"] . '</a>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+    </div>
 
+    <script src="/bootstrap4_6_2/js/jquery.slim.min.js"></script>
+    <script src="/bootstrap4_6_2/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
---
+++ www/old index not optimal.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+     <title>PHP Form with PDO</title>
+
+     <!-- Bootstrap CSS -->
+     <!-- <link rel="stylesheet" https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"> -->
+     <link href="/bootstrap4_6_2/css/bootstrap.min.css" rel="stylesheet">
+     <link rel="stylesheet" href="/bootstrap4_6_2/css/fontawesome-free-5.15.4-web/css/all.css">
+
+     <!-- Подключение стилей из отдельного файла -->
+     <link rel="stylesheet" href="css/styles.css">
+
+
+</head>
+
+<body>
+     <div class="container">
+          <h1 class="mb-3 mt-3">Test PHP code</h1>
+          <div class="container row">
+               <div class="p-1">
+                    <div class="card" style="width: 18rem;">
+                         <a href="/site1">
+                              <img src="img/crud1.png" class="card-img-top p-1" alt="...">
+                         </a>
+                         <div class="card-body">
+                              <h5 class="card-title">
+                                   <a href="/site1" class="card-link text-dark">Add to database</a>
+                              </h5>
+                              <p class="card-text">Adding and viewing data in the database</p>
+                              <a href="/site1" class="btn btn-primary">Example 1</a>
+                         </div>
+                    </div>
+               </div>
+               <div class="p-1">
+                    <div class="card" style="width: 18rem;">
+                         <a href="/site2">
+                              <img src="img/crud2.png" class="card-img-top img-fluid p-1" alt="...">
+                         </a>
+                         <div class="card-body">
+                              <h5 class="card-title">
+                                   <a href="/site2" class="card-link text-dark">+++ CRUD</a>
+                              </h5>
+                              <p class="card-text">Create, Read, Update, Delete operations in the database.</p>
+                              <a href="/site2" class="btn btn-primary">Example 2</a>
+                         </div>
+                    </div>
+               </div>
+               <div class="p-1">
+                    <div class="card" style="width: 18rem;">
+                         <a href="/site3">
+                              <img src="https://dummyimage.com/300x200/6824b5/ffffff" class="card-img-top" alt="...">
+                         </a>
+                         <div class="card-body">
+                              <h5 class="card-title">Authentication authorization</h5>
+                              <p class="card-text">old, doesn't work Authentication authorization database</p>
+                              <a href="/site3" class="btn btn-primary">Example 3</a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+          <div class="container row">
+               <div class="p-1">
+                    <div class="card" style="width: 18rem;">
+                         <a href="/site4">
+                              <img src="img/di.png" class="card-img-top p-1" alt="...">
+                         </a>
+                         <div class="card-body">
+                              <h5 class="card-title">Внедрение зависимостей DI</h5>
+                              <p class="card-text">Простой пример с использованием MySql</p>
+                              <a href="/site4" class="btn btn-primary">Example 4</a>
+                         </div>
+                    </div>
+               </div>
+
+               <div class="p-1">
+                    <div class="card" style="width: 18rem;">
+                         <a href="/site5">
+                              <img src="img/crud53.png" class="card-img-top p-1" alt="...">
+                         </a>
+                         <div class="card-body">
+                              <h5 class="card-title">+++ Авторизация и регистрация</h5>
+                              <p class="card-text">Авторизация регистрация на чистом PHP, пример</p>
+                              <a href="/site5" class="btn btn-primary">Example 5</a>
+                         </div>
+                    </div>
+               </div>
+
+               <div class="p-1">
+                    <div class="card" style="width: 18rem;">
+                         <a href="/site6">
+                              <img src="https://dummyimage.com/300x200/6824b5/ffffff" class="card-img-top" alt="...">
+                         </a>
+                         <div class="card-body">
+                              <h5 class="card-title">Card title</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="#" class="btn btn-primary">Go somewhere</a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+     </div>
+
+     <script src="/bootstrap4_6_2/js/jquery.slim.min.js"></script>
+     <script src="/bootstrap4_6_2/js/bootstrap.bundle.min.js"></script>
+
+     <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
+     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script> -->
+
+</body>
+
+</html>
+
+
+
