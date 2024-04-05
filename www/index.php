f814cfad1845b20bfbc41e3d7a21cb5ba284a610
Aleksandr <sh17aleksandr@gmail.com>
2024-04-05 14:43:12 +0300

    index php for row col

--- www/index.php
+++ www/index.php
@@ -16,73 +16,82 @@
     <div class="container">
         <h1 class="mb-3 mt-3">Test PHP code</h1>
 
-        <div class="container row">
-            <?php
-            // Массив данных для карточек
-            $cards = array(
-                array(
-                    "url" => "/site1",
-                    "img_src" => "img/crud1.png",
-                    "title" => "Add to database",
-                    "text" => "Adding and viewing data in the database",
-                    "btn_text" => "Example 1"
-                ),
-                array(
-                    "url" => "/site2",
-                    "img_src" => "img/crud2.png",
-                    "title" => "+++ CRUD",
-                    "text" => "Create, Read, Update, Delete operations in the database.",
-                    "btn_text" => "Example 2"
-                ),
-                array(
-                    "url" => "/site3",
-                    "img_src" => "https://dummyimage.com/300x200/6824b5/ffffff",
-                    "title" => "Authentication authorization",
-                    "text" => "old, doesn't work Authentication authorization database",
-                    "btn_text" => "Example 3"
-                ),
-                array(
-                    "url" => "/site4",
-                    "img_src" => "img/di.png",
-                    "title" => "Внедрение зависимостей DI",
-                    "text" => "Простой пример с использованием MySql",
-                    "btn_text" => "Example 4"
-                ),
-                array(
-                    "url" => "/site5",
-                    "img_src" => "img/crud53.png",
-                    "title" => "+++ Авторизация и регистрация",
-                    "text" => "Авторизация регистрация на чистом PHP, пример",
-                    "btn_text" => "Example 5"
-                ),
-                array(
-                    "url" => "/site6",
-                    "img_src" => "https://dummyimage.com/300x200/6824b5/ffffff",
-                    "title" => "Card title",
-                    "text" => "Some quick example text to build on the card title and make up the bulk of the card's content.",
-                    "btn_text" => "Go somewhere"
-                )
-            );
+        <?php
+        // Массив данных для карточек
+        $cards = array(
+            array(
+                "url" => "/site1",
+                "img_src" => "img/crud1.png",
+                "title" => "Add to database",
+                "text" => "Adding and viewing data in the database",
+                "btn_text" => "Example 1"
+            ),
+            array(
+                "url" => "/site2",
+                "img_src" => "img/crud2.png",
+                "title" => "+++ CRUD",
+                "text" => "Create, Read, Update, Delete operations in the database.",
+                "btn_text" => "Example 2"
+            ),
+            array(
+                "url" => "/site3",
+                "img_src" => "https://dummyimage.com/300x200/6824b5/ffffff",
+                "title" => "Authentication authorization",
+                "text" => "old, doesn't work Authentication authorization database",
+                "btn_text" => "Example 3"
+            ),
+            array(
+                "url" => "/site4",
+                "img_src" => "img/di.png",
+                "title" => "Внедрение зависимостей DI",
+                "text" => "Простой пример с использованием MySql",
+                "btn_text" => "Example 4"
+            ),
+            array(
+                "url" => "/site5",
+                "img_src" => "img/crud53.png",
+                "title" => "+++ Авторизация и регистрация",
+                "text" => "Авторизация регистрация на чистом PHP, пример",
+                "btn_text" => "Example 5"
+            ),
+            array(
+                "url" => "/site6",
+                "img_src" => "https://dummyimage.com/300x200/6824b5/ffffff",
+                "title" => "Card title",
+                "text" => "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                "btn_text" => "Go somewhere"
+            )
+        );
 
-            // Цикл для создания карточек
-            foreach ($cards as $card) {
-                echo '<div class="p-1">';
-                echo '<div class="card" style="width: 18rem;">';
-                echo '<a href="' . $card["url"] . '">';
-                echo '<img src="' . $card["img_src"] . '" class="card-img-top p-1" alt="...">';
-                echo '</a>';
-                echo '<div class="card-body">';
-                echo '<h5 class="card-title">';
-                echo '<a href="' . $card["url"] . '" class="card-link text-dark">' . $card["title"] . '</a>';
-                echo '</h5>';
-                echo '<p class="card-text">' . $card["text"] . '</p>';
-                echo '<a href="' . $card["url"] . '" class="btn btn-primary">' . $card["btn_text"] . '</a>';
-                echo '</div>';
-                echo '</div>';
-                echo '</div>';
+        // Распределение карточек по три столбцам и двум строкам
+        $rowCount = ceil(count($cards) / 3);
+        $counter = 0;
+
+        // Цикл для создания строк и столбцов карточек
+        for ($row = 1; $row <= $rowCount; $row++) {
+            echo '<div class="row">'; // Начало строки
+            for ($col = 1; $col <= 3; $col++) {
+                if ($counter < count($cards)) {
+                    echo '<div class="col-md-3">';
+                    echo '<div class="card mb-3">';
+                    echo '<a href="' . $cards[$counter]["url"] . '">';
+                    echo '<img src="' . $cards[$counter]["img_src"] . '" class="card-img-top" alt="...">';
+                    echo '</a>';
+                    echo '<div class="card-body">';
+                    echo '<h5 class="card-title">';
+                    echo '<a href="' . $cards[$counter]["url"] . '" class="card-link text-dark">' . $cards[$counter]["title"] . '</a>';
+                    echo '</h5>';
+                    echo '<p class="card-text">' . $cards[$counter]["text"] . '</p>';
+                    echo '<a href="' . $cards[$counter]["url"] . '" class="btn btn-primary">' . $cards[$counter]["btn_text"] . '</a>';
+                    echo '</div>';
+                    echo '</div>';
+                    echo '</div>';
+                    $counter++;
+                }
             }
-            ?>
-        </div>
+            echo '</div>'; // Завершение строки
+        }
+        ?>
     </div>
 
     <script src="/bootstrap4_6_2/js/jquery.slim.min.js"></script>
